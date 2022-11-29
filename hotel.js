e2f491a4c6cb04b096aeee91a2248ef2a31dde82
kkkdkdkdkd <114073825+kkkdkdkdkd@users.noreply.github.com>
2022-11-29 23:34:32 +0900

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,15 @@
+function mouseEnter(){
+    document.getElementById("Menu1").style.color = "gray";
+    // document.getElementById("Menu2").style.color = "gray";
+    // document.getElementById("Menu3").style.color = "gray";
+    // document.getElementById("Menu4").style.color = "gray";
+}
+
+function mouseLeave(){
+    document.getElementById("Menu1").style.color = "black";
+    // document.getElementById("Menu2").style.color = "black";
+    // document.getElementById("Menu3").style.color = "black";
+    // document.getElementById("Menu4").style.color = "black";
+}
+
+
